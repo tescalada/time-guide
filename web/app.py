33349077eed6a0b1fab5af19f5fbbc20198af6b6
Tristan Escalada <tristan@escalada.us>
2019-06-18 16:59:29 -0400

    Adding an example using request args
    added an example that uses the request args to be more dynamic

--- web/app.py
+++ web/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -9,11 +9,46 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/jupiter')
-def jupiter_info():
+@app.route('/info')
+def planet_info():
+
+    # get the name of the planet passed in
+    # via the url at /info?planet=PLANETNAME
+    planet = request.args.get("planet")
+
+    # some data about the planets
+    planet_data = {
+        "mercury": {
+            "size": 2440,
+        },
+        "venus": {
+            "size": 6052,
+        },
+        "earth": {
+            "size": 6371,
+        },
+        "mars": {
+            "size": 3390,
+        },
+        "jupiter": {
+            "size": 69911,
+        },
+        "saturn": {
+            "size": 58232,
+        },
+        "uranus": {
+            "size": 25362,
+        },
+        "neptune": {
+            "size": 24622,
+        },
+    }
+
+    # return json with the planet name and the
+    # size looked up from the panet_data dict
     return jsonify(
-        name='Jupiter',
-        size='huge',
+        name=planet,
+        size=planet_data[planet]['size'],
     )
 
 
