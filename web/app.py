3f81eb7ec3e66f32c6314990cf7c47f4f16e2b76
Tristan Escalada <tristan@escalada.us>
2019-06-18 16:48:25 -0400

    Adding a json response example
    added an endpoint that will return json

--- web/app.py
+++ web/app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+
+from flask import Flask, jsonify
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/jupiter')
+def jupiter_info():
+    return jsonify(
+        name='Jupiter',
+        size='huge',
+    )
+
 
 if __name__ == '__main__':
     app.run()
