2a321b996ef22d031f36248684a5806781f2effc
Daniel Schnitzer <dakschnitzer@gmail.com>
2019-08-12 10:09:00 -0400

    Update main.py

--- embedded/esp32/planet_scale/main.py
+++ embedded/esp32/planet_scale/main.py
@@ -36,6 +36,8 @@ r = RotaryIRQ(pin_num_clk=14,
 sdist_i = [0.417, 0.723, 1.64, 5.3, 10.1, 19.8, 29.9]
 
 names = ['Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+menu = ['Planet Menu', 'Sky Location', 'Sky Chart', 'Orbital Data']
+
 
 # create log scale of sizes of planets
 arb = 0.9
@@ -68,6 +70,34 @@ oled.show()
 
 lastval = r.value()
 
+oled.fill(0)
+
+# make huge sun
+oled.vline(0, 0, 64, 1)
+oled.vline(1, 0, 64, 1)
+oled.vline(2, 0, 64, 1)
+oled.vline(3, 0, 64, 1)
+oled.vline(4, 0, 64, 1)
+oled.vline(5, 0, 64, 1)
+oled.vline(6, 0, 64, 1)
+oled.vline(7, 0, 64, 1)
+oled.vline(8, 3, 58, 1)
+oled.vline(9, 16, 33, 1)
+oled.vline(10, 23, 19, 1)
+
+# show planet index graphic
+graphics.circle(sdist_i[0], 31, pre[0], 1)
+graphics.circle(sdist_i[1], 31, pre[1], 1)
+graphics.fill_circle(sdist_i[2], 31, pre[2], 1)
+graphics.circle(sdist_i[3], 31, pre[3], 1)
+graphics.circle(sdist_i[4], 31, pre[4], 1)
+graphics.circle(sdist_i[5], 31, pre[5], 1)
+oled.line(sdist_i[5] - pre[5], 37, sdist_i[5] + pre[5], 25, 1)
+graphics.circle(sdist_i[6], 31, pre[6], 1)
+graphics.circle(sdist_i[7], 31, pre[7], 1)
+oled.show()
+
+
 while True:
 
     oled.fill(0)
@@ -98,6 +128,8 @@ while True:
 
     if not button.value():
         print('Button pressed!')
+        oled.fill(0)
+        oled.show()
 
     utime.sleep_ms(125)
 
