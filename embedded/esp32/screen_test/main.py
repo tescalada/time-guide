e48cbcb8fe80773af690e4b273491069c53cc5aa
Daniel Schnitzer <dakschnitzer@gmail.com>
2019-08-25 18:12:07 -0400

    Update main.py
    working with two external displays

--- embedded/esp32/screen_test/main.py
+++ embedded/esp32/screen_test/main.py
@@ -8,15 +8,21 @@ import gfx
 from machine import I2C, Pin, SPI
 
 oled_reset_pin = Pin(16, Pin.OUT)
-oled_reset_pin.value(1)
+#oled_reset_pin.value(1)
 
-spi = SPI(1, baudrate=20000000)
+#spi = SPI(1, baudrate=80000000)
+hspi = SPI(1, 10000000, sck=Pin(18), mosi=Pin(23), miso=Pin(19))
 
-display = sh1106.SH1106_SPI(128, 64, spi, dc=Pin(26), res=oled_reset_pin, cs=Pin(5))
+
+display = sh1106.SH1106_SPI(128, 64, hspi, dc=Pin(26), res=oled_reset_pin, cs=Pin(5))
+display2 = sh1106.SH1106_SPI(128, 64, hspi, dc=Pin(33), res=oled_reset_pin, cs=Pin(2))
+
 utime.sleep(1)
 
 display.sleep(False)
 display.rotate(1)
+display2.sleep(False)
+display2.rotate(1)
 utime.sleep(1)
 
 i2c = I2C(scl=Pin(15), sda=Pin(4))
@@ -26,6 +32,8 @@ utime.sleep(1)
 
 graphics = gfx.GFX(128, 64, oled.pixel)
 graphics1 = gfx.GFX(128, 64, display.pixel)
+graphics2 = gfx.GFX(128, 64, display2.pixel)
+
 
 oled.fill(0)
 oled.text('oled init',0,0,1)
@@ -35,8 +43,13 @@ display.fill(0)
 display.text('display init',0,0,1)
 graphics1.circle(63, 32, 10, 1)
 
+display2.fill(0)
+display2.text('display2 init',0,0,1)
+graphics2.circle(63, 32, 10, 1)
+
 oled.show()
 display.show()
+display2.show()
 
 utime.sleep(2)
 
