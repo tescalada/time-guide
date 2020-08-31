b0df697bfe2c1ded562c4548f0a5c6425869eafd
dan <51975338+dakschnitzer@users.noreply.github.com>
2020-08-31 13:50:53 -0400

    changed USB port name for new computer
    yes

--- embedded/modules/reflash_script.py
+++ embedded/modules/reflash_script.py
@@ -1,34 +1,34 @@
 # for esp8266 board
 wget http://micropython.org/resources/firmware/esp8266-20190529-v1.11.bin
-esptool.py --port /dev/tty.SLAB_USBtoUART erase_flash
-esptool.py --port /dev/tty.SLAB_USBtoUART --baud 460800 write_flash --flash_size=detect 0 esp8266-20190529-v1.11.bin
+esptool.py --port /dev/tty.usbserial-0001 erase_flash
+esptool.py --port /dev/tty.usbserial-0001 --baud 460800 write_flash --flash_size=detect 0 esp8266-20190529-v1.11.bin
 
 # for esp32 board
 wget http://micropython.org/resources/firmware/esp32-20190529-v1.11.bin
-esptool.py --chip esp32 --port /dev/tty.SLAB_USBtoUART erase_flash
-esptool.py --chip esp32 --port /dev/tty.SLAB_USBtoUART --baud 460800 write_flash -z 0x1000 esp32-20190529-v1.11.bin
+esptool.py --chip esp32 --port /dev/tty.usbserial-0001 erase_flash
+esptool.py --chip esp32 --port /dev/tty.usbserial-0001 --baud 460800 write_flash -z 0x1000 esp32-20190529-v1.11.bin
 
 # put modules
 cd Documents/GitHub/time-guide/embedded/modules
-ampy --port /dev/tty.SLAB_USBtoUART --baud 115200 put ssd1306.py
-ampy --port /dev/tty.SLAB_USBtoUART --baud 115200 put sh1106.py
-ampy --port /dev/tty.SLAB_USBtoUART --baud 115200 put gfx.py
-ampy --port /dev/tty.SLAB_USBtoUART --baud 115200 put PlanetFn.py
-ampy --port /dev/tty.SLAB_USBtoUART --baud 115200 put shapes3d.py
+ampy --port /dev/tty.usbserial-0001 --baud 115200 put ssd1306.py
+ampy --port /dev/tty.usbserial-0001 --baud 115200 put sh1106.py
+ampy --port /dev/tty.usbserial-0001 --baud 115200 put gfx.py
+ampy --port /dev/tty.usbserial-0001 --baud 115200 put PlanetFn.py
+ampy --port /dev/tty.usbserial-0001 --baud 115200 put shapes3d.py
 
 
 cd micropython-rotary-master
-ampy --port /dev/tty.SLAB_USBtoUART --baud 115200 put rotary.py
-ampy --port /dev/tty.SLAB_USBtoUART --baud 115200 put rotary_irq_esp.py
+ampy --port /dev/tty.usbserial-0001 --baud 115200 put rotary.py
+ampy --port /dev/tty.usbserial-0001 --baud 115200 put rotary_irq_esp.py
 
 cd ..
 cd ..
 cd esp32/planet_scale
-ampy --port /dev/tty.SLAB_USBtoUART --baud 115200 put main.py
-ampy --port /dev/tty.SLAB_USBtoUART --baud 115200 put credentials.py
+ampy --port /dev/tty.usbserial-0001 --baud 115200 put main.py
+ampy --port /dev/tty.usbserial-0001 --baud 115200 put credentials.py
 
 
-screen /dev/tty.SLAB_USBtoUART 115200
+screen /dev/tty.usbserial-0001 115200
 # in screen, hit CTRL-A, k, y to quit and return to terminal
 
 
