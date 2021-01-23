653d8e8f4ffab01418df3ea004bc1c1778a6bc2d
dan <51975338+dakschnitzer@users.noreply.github.com>
2021-01-22 20:33:36 -0500

    Update main.py

--- embedded/esp32/planet_music/LEDs/main.py
+++ embedded/esp32/planet_music/LEDs/main.py
@@ -6,7 +6,7 @@ import utime as time
 import urequests as requests
 from credentials import WIFI_SSID, WIFI_PASSWORD, WOLFRAM_API_KEY
 from ntptime import settime
-
+from scron.week import simple_cron
 
 def do_connect():
     wlan = network.WLAN(network.STA_IF)
@@ -22,67 +22,67 @@ do_connect()
 gc.collect()
 
 time.sleep(1)
+
+def make_planet_list():
+    for i, name in enumerate (names) :
+        # obtain rise time from wolframalpha API
+        url = "http://api.wolframalpha.com/v2/result?i={0}%20rise%20today%20unix%20time%3F&appid={1}".format(name, WOLFRAM_API_KEY)
+        r = requests.get(url)
+        print(r.text)
+        rise[i][1] = r.text
+        rise[i][1] = [x.strip() for x in rise[i][1].split(' ')]
+        rise[i][1] = int(rise[i][1][0]) - 946080000 #convert to embedded Epoch
+        r.close()
+        del r
+        rise[i][0] = rise[i][1] - now #seconds from "now" until event
+        rise[i][2] = name
+        rise [i][3] = 'rise'
+
+        # obtain set time from wolframalpha API
+        url = "http://api.wolframalpha.com/v2/result?i={0}%20set%20today%20unix%20time%3F&appid={1}".format(name, WOLFRAM_API_KEY)
+        r = requests.get(url)
+        print(r.text)
+        sett[i][1] = r.text
+        sett[i][1] = [x.strip() for x in sett[i][1].split(' ')]
+        sett[i][1] = int(sett[i][1][0]) - 946080000 #convert to embedded Epoch
+        r.close()
+        del r
+        sett[i][0] = sett[i][1] - now #seconds from "now" until event
+        sett[i][2] = name
+        sett [i][3] = 'sett'
+
+    planet_list = rise.append(sett)
+
+# define variables
+names = ['Sun', 'Moon']
+# 'Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'
+rise = [[0 for i in range(4)] for j in range(len(names))]
+sett = [[0 for i in range(4)] for j in range(len(names))]
+
+# set the RTC
 settime()
 
-now = time.localtime(time.mktime(time.localtime()) - 5*3600)
+simple_cron.run()
+# set the time once every hour
+simple_cron.add(
+    'Hourly',
+    lambda *a,**k: settime(),
+    hours=0,
+    minutes=0,
+    seconds=0
+)
+
+# return time since the Epoch (embedded)
+now = int(time.time())
 print(now)
 time.sleep(1)
 
+make_planet_list()
 
-# this file will get the day's rise/set times and light up LEDs for that day
+planet_list
 
-names = ['Sun', 'Moon', 'Mercury', 'Venus', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
-rise = [0 for i in range(len(names))]
-sett = [0 for i in range(len(names))]
-
-
-for index1, name in enumerate (names) :
-
-    # obtain rise time from wolframalpha API
-    url = "http://api.wolframalpha.com/v1/result?i={0}%20rise%3F&appid={1}".format(name, WOLFRAM_API_KEY)
-    r = requests.get(url)
-    print(r.text)
-    rise[index1] = r.text
-    r.close()
-    del r
-
-    # obtain set time from wolframalpha API
-    url = "http://api.wolframalpha.com/v1/result?i={0}%20set%3F&appid={1}".format(name, WOLFRAM_API_KEY)
-    r = requests.get(url)
-    print(r.text)
-    sett[index1] = r.text
-    r.close()
-    del r
-
-
-#parse sunrise strings, which start with "at "
-#parse string by spaces
-rise[0] = [x.strip() for x in rise[0].split(' ')]
-sett[0] = [x.strip() for x in sett[0].split(' ')]
-#delete "at " from list
-del rise[0][0]
-del sett[0][0]
-
-#parse all other strings
-for i in range(len(names) - 1):
-    #parse string by spaces
-    rise[i+1] = [x.strip() for x in rise[i+1].split(' ')]
-    sett[i+1] = [x.strip() for x in sett[i+1].split(' ')]
-
-#parse whole list, convert to 24h time and keep only the hour and minute
-for i in range(len(names)):
-    if rise[i][1] == 'A.M.':
-        rise[i] = rise[i][0] + ' AM'
-    else:
-        rise[i] = rise[i][0] + ' PM'
-
-    if sett[i][1] == 'A.M.':
-        sett[i] = sett[i][0] + ' AM'
-    else:
-        sett[i] = sett[i][0] + ' PM'
-
-    # rise[i] = datetime.strptime(rise[i], "%I:%M %p")
-    # sett[i] = datetime.strptime(sett[i], "%I:%M %p")
-
-    # rise[i] = int(datetime.strftime(rise[i], "%H"))
-    # sett[i] = int(datetime.strftime(sett[i], "%H"))
+sorted_planet_time_list = [(12345, "mars", "rise")]
+for timestamp, planetname, action in sorted_planet_time_list:
+    # sleep until timestamp
+    if planetname == "mars" and action == "rise":
+        #whatever
